62de30c007b999ece70006c69b328889519ff68d
AnarAgaev <anar.n.agaev@gmail.com>
2026-05-09 10:13:41 +0300

    reafactor: update async func into the store

--- store.ts
+++ store.ts
@@ -1,4 +1,5 @@
 import {create} from 'zustand'
+import {getAllPosts, getPostsBySearch} from '@/helpers'
 import type {PostsStore} from '@/types'
 
 const usePosts = create<PostsStore>()((set, _get) => ({
@@ -12,47 +13,13 @@ const usePosts = create<PostsStore>()((set, _get) => ({
 
 	getAllPosts: async () => {
 		set({loading: true})
-
-		const response = await fetch(
-			'https://jsonplaceholder.typicode.com/posts?_limit=20',
-			{
-				next: {
-					revalidate: 60,
-				},
-			},
-		)
-
-		if (!response.ok) {
-			set({loading: false, isError: true})
-			return
-			// throw new Error('Enable to fetch post list!')
-		}
-
-		const posts = await response.json()
-
+		const posts = await getAllPosts()
 		set({posts, loading: false})
 	},
 
 	getPostsBySearch: async ({query}) => {
 		set({loading: true})
-
-		const response = await fetch(
-			`https://jsonplaceholder.typicode.com/posts?q=${encodeURIComponent(query)}`,
-			{
-				next: {
-					revalidate: 60,
-				},
-			},
-		)
-
-		if (!response.ok) {
-			set({loading: false, isError: true})
-			return
-			// throw new Error('Enable to fetch post list by search query!')
-		}
-
-		const posts = await response.json()
-
+		const posts = await getPostsBySearch(query)
 		set({posts, loading: false})
 	},
 }))
